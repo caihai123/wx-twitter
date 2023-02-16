d8b17d674ce9a3b117f821991e51642ad28b325e
蔡海 <ch15519060282@163.com>
2023-02-16 23:11:40 +0800

    【优化】优化创建圈子后的逻辑

--- web/pages/create-tribe/index.js
+++ web/pages/create-tribe/index.js
@@ -1,7 +1,7 @@
 // pages/create-tribe/index.js
 import dayjs from "dayjs";
 const app = getApp();
-
+const db = wx.cloud.database();
 Page({
   /**
    * 页面的初始数据
@@ -27,32 +27,75 @@ Page({
   },
 
   submitForm() {
-    const db = wx.cloud.database();
+    const that = this;
     const { name, description } = this.data;
     if (!name) {
       wx.showToast({
         title: "请填写名称",
         icon: "error",
       });
-      return
+      return;
     }
+    const { userInfo, tribeList } = app.store.getState();
+    const formData = {
+      name: name,
+      description: description,
+      createTime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      members: [userInfo._id],
+    };
     db.collection("tribes").add({
-      data: {
-        name: name,
-        description: description,
-        createTime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-        members: [app.globalData.userInfo._id],
-      },
-      success: function () {
-        wx.showToast({
-          title: "创建成功",
-          icon: "success",
-          duration: 2000,
+      data: formData,
+      success: function (res) {
+        const { _id } = res;
+        app.store.dispatch({
+          type: "setTribeList",
+          payload: [{ _id, ...formData }, ...tribeList],
         });
-        setTimeout(() => {
-          wx.navigateBack();
-        }, 2000);
+        if (tribeList.length) {
+          // 询问用户是否切换
+          wx.showModal({
+            title: "提示",
+            content: "圈子创建成功，是否立即切换？",
+            complete: (res) => {
+              if (res.cancel) {
+                that.updateCurrentTribe(_id).then(() => {
+                  wx.navigateBack();
+                });
+              } else if (res.confirm) {
+                wx.navigateBack();
+              }
+            },
+          });
+        } else {
+          // 如果是第一个圈子则自动帮他切换
+          that.updateCurrentTribe(_id);
+          wx.showToast({
+            title: "创建成功",
+            icon: "success",
+            duration: 2000,
+          });
+          setTimeout(() => {
+            wx.navigateBack();
+          }, 2000);
+        }
       },
     });
+  },
+
+  // 更新用户当前的群
+  updateCurrentTribe(currentTribeId) {
+    const { userInfo } = app.store.getState();
+    return db
+      .collection("user")
+      .doc(userInfo._id)
+      .update({
+        data: { currentTribe: currentTribeId },
+        success: () => {
+          this.store.dispatch({
+            type: "setUserInfo",
+            payload: { ...userInfo, currentTribe: currentTribeId },
+          });
+        },
+      });
   },
 });
