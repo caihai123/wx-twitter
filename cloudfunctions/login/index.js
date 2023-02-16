6afda027dc1fb459f6ff19ef245eb6711c86d6d7
蔡海 <ch15519060282@163.com>
2023-02-16 21:41:18 +0800

    【更新】登录接口更新

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,15 +1,47 @@
-// 云函数入口文件
+/**
+ * 小程序登录
+ * 第一次进入时在用户表创建数据
+ * return userInfo
+ */
+const dayjs = require("dayjs");
 const cloud = require("wx-server-sdk");
-
 cloud.init();
+const db = cloud.database();
+const _ = db.command;
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();
-  return {
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-    env: wxContext.ENV,
+  const { OPENID } = cloud.getWXContext();
+
+  const { data: userList } = await db
+    .collection("user")
+    .where({ _openid: OPENID })
+    .field({ createTime: false })
+    .limit(1)
+    .get();
+
+  const [userInfo] = userList;
+
+  if (userInfo) {
+    const { data: tribeList } = await db
+      .collection("tribes")
+      .where({ members: _.all([userInfo._id]) })
+      .field({ members: false })
+      .get();
+    return {
+      ...userInfo,
+      tribeList,
+    };
+  } else {
+    // 如果不存在则帮他创建之后再返回
+    await db.collection("user").add({
+      data: {
+        _openid: OPENID,
+        createTime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      },
+    });
+    return {
+      _openid: OPENID,
+    };
   }
 };
