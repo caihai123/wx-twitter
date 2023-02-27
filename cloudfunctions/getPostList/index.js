0521b7661f4dd3ec60d7b9b03cd1319ff2e0b697
caihai <ch15519060282@163.com>
2023-02-27 14:49:25 +0800

    【更新】只获取自己及关注用户的动态

--- cloudfunctions/getPostList/index.js
+++ cloudfunctions/getPostList/index.js
@@ -15,24 +15,42 @@ exports.main = async (event, context) => {
     .collection("user")
     .where({ _openid: wxContext.OPENID })
     .field({ tribes: true })
+    .limit(1)
     .get();
   const userInfo = userList[0];
+
+  // 获取关注者列表
+  const { data: followList } = await db
+    .collection("follow")
+    .where({
+      userId: userInfo._id,
+    })
+    .get();
 
   if (userInfo) {
     const { list } = await db
       .collection("posts")
       .aggregate()
+      .match({
+        // 查询自己和关注者的动态
+        userId: _.in([
+          ...followList.map((item) => item.followId),
+          userInfo._id,
+        ]),
+      })
       .lookup({
         from: "user",
+        // localField: "userId",
+        // foreignField: "_id",
+        let: { userId: "$userId" },
         as: "user",
-        let: { openid: "$_openid" },
         pipeline: $.pipeline()
-          .match(_.expr($.and([$.eq(["$_openid", "$$openid"])])))
+          .match(_.expr($.eq(["$_id", "$$userId"])))
           .project({
+            _id: true,
             nickName: true,
             avatarUrl: true,
             describe: true,
-            gender: true,
           })
           .limit(1)
           .done(),
