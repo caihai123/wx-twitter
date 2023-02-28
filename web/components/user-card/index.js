a3e33f7cef0d6bfaaadac542c374c0a7e190a2fa
caihai <ch15519060282@163.com>
2023-02-28 16:26:51 +0800

    【优化】在本地先计算操作后的状态 增加响应速度

--- web/components/user-card/index.js
+++ web/components/user-card/index.js
@@ -1,4 +1,6 @@
 // components/user-card/index.js
+const app = getApp();
+
 Component({
   /**
    * 组件的属性列表
@@ -9,17 +11,63 @@ Component({
   properties: {
     userId: String,
     nickName: String,
-    avatarUrl:String,
+    avatarUrl: String,
     describe: String,
+    followState: String,
+  },
+
+  observers: {
+    followState: function (val) {
+      this.setData({
+        _followState: val,
+      });
+    },
   },
 
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    _followState: "",
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    // 切换对某个人的关注状态
+    followSwitch() {
+      const { userId } = this.properties;
+      const { _followState } = this.data;
+
+      switch (_followState) {
+        case "1":
+          this.setData({ _followState: "4" });
+          break;
+        case "2":
+          this.setData({ _followState: "3" });
+          break;
+        case "3":
+          this.setData({ _followState: "2" });
+          break;
+        case "4":
+          this.setData({ _followState: "1" });
+          break;
+        default:
+          break;
+      }
+
+      wx.cloud.callFunction({
+        name: "handleFollow",
+        data: { userId, type: "followSwitch" },
+        success: ({ result }) => {
+          this.triggerEvent("followChange", {
+            userId,
+            followState: result.followState,
+          });
+          app.login();
+        },
+      });
+    },
+  },
 });
