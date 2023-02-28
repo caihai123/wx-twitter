19aa5c9b756cffc155f422ee911f9a7fd7c13ba8
caihai <ch15519060282@163.com>
2023-02-28 11:27:16 +0800

    【更新】返回是否喜欢的标识

--- cloudfunctions/getPostList/index.js
+++ cloudfunctions/getPostList/index.js
@@ -3,40 +3,44 @@ const cloud = require("wx-server-sdk");
 
 cloud.init();
 
+const db = cloud.database();
+const _ = db.command;
+const $ = db.command.aggregate;
+
+console.log($);
+
+// 获取用户Id
+const getUserId = async (openid) => {
+  const { data: userList } = await db
+    .collection("user")
+    .where({ _openid: openid })
+    .field({ _id: true })
+    .limit(1)
+    .get();
+
+  return userList[0]._id;
+};
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext();
-  const db = cloud.database();
-  const _ = db.command;
-  const $ = db.command.aggregate;
 
-  // 获取自己的用户信息
-  const { data: userList } = await db
-    .collection("user")
-    .where({ _openid: wxContext.OPENID })
-    .field({ tribes: true })
-    .limit(1)
-    .get();
-  const userInfo = userList[0];
+  // 获取自己的用户ID
+  const userId = await getUserId(wxContext.OPENID);
 
-  // 获取关注者列表
-  const { data: followList } = await db
-    .collection("follow")
-    .where({
-      userId: userInfo._id,
-    })
-    .get();
+  if (userId) {
+    // 获取关注者列表
+    const { data: followList } = await db
+      .collection("follow")
+      .where({ userId })
+      .get();
 
-  if (userInfo) {
     const { list } = await db
       .collection("posts")
       .aggregate()
       .match({
         // 查询自己和关注者的动态
-        userId: _.in([
-          ...followList.map((item) => item.followId),
-          userInfo._id,
-        ]),
+        userId: _.in([...followList.map((item) => item.followId), userId]),
       })
       // 获取此动态的用户信息
       .lookup({
@@ -68,12 +72,18 @@ exports.main = async (event, context) => {
       })
       .addFields({
         user: $.arrayElemAt(["$user", 0]),
-        heartNum: $.size("$heart"), // 喜欢的人的数量
-        isHeart: $.in([userInfo._id, "$heart"]), // 自己是否喜欢
+        heartNum: $.size("$heart"),
       })
-      .project({ _openid: false, heart: false })
+      .project({ _openid: false })
       .end();
-    return list;
+    return list.map((item) => {
+      const { heart, ...rest } = item;
+      return {
+        // 自己是否喜欢
+        isHeart: heart.some((item) => item.userId === userId),
+        ...rest,
+      };
+    });
   } else {
     return {
       errMsg: "当前用户不存在",
