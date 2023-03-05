707dd5d18a264b38e36c1ba7204f2d262cfdb559
蔡海 <ch15519060282@163.com>
2023-03-05 23:54:39 +0800

    【新增】新增媒体tab功能

---
+++ web/pages/user-page/components/tab3/index.js
@@ -0,0 +1,45 @@
+// pages/user-page/components/tab1/index.js
+const db = wx.cloud.database();
+const _ = db.command;
+const $ = _.aggregate;
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userId: String,
+  },
+
+  observers: {
+    userId: function (userId) {
+      if (userId) {
+        this.getPostList();
+      }
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    postList: [],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    async getPostList() {
+      const { userId } = this.properties;
+      const { data } = await db
+        .collection("posts")
+        .orderBy("createTime", "desc")
+        .where({ userId, 'imgList.0': _.exists(true) })
+        .limit(10)
+        .field({ _id: true })
+        .get();
+      this.setData({ postList: data });
+    },
+  },
+});
