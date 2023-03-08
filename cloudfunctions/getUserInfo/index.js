56d47a7f3deda8053ef44664c0beff325bf2ecfa
caihai <ch15519060282@163.com>
2023-03-08 14:29:26 +0800

    【更新】获取用户接口，返回和我的好友状态

--- cloudfunctions/getUserInfo/index.js
+++ cloudfunctions/getUserInfo/index.js
@@ -1,9 +1,22 @@
-// 云函数入口文件
+// 通过id获取用户信息
+
 const cloud = require("wx-server-sdk");
 
 cloud.init();
 
 const db = cloud.database();
+
+// 获取用户Id
+const getUserId = async (openid) => {
+  const { data: userList } = await db
+    .collection("user")
+    .where({ _openid: openid })
+    .field({ _id: true })
+    .limit(1)
+    .get();
+
+  return userList[0]._id;
+};
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -11,18 +24,39 @@ exports.main = async (event, context) => {
   const { id } = event;
   // 获取用户基础信息
   const { data: userInfo } = await db.collection("user").doc(id).get();
-  console.log(userInfo)
 
   // 获取好友数量
-  const [{ total: follow }, { total: fans }] = await Promise.all([
+  const [{ total: followNum }, { total: fansNum }] = await Promise.all([
     db.collection("follow").where({ userId: id }).count(),
     db.collection("follow").where({ followId: id }).count(),
   ]);
 
+  const selfUserId = await getUserId(wxContext.OPENID);
+
+  // 我是否关注他
+  const isFollow = await (async () => {
+    const { total } = await db
+      .collection("follow")
+      .where({ userId: selfUserId, followId: id })
+      .count();
+    return total > 0;
+  })();
+
+  // 他是否关注我
+  const isFans = await (async () => {
+    const { total } = await db
+      .collection("follow")
+      .where({ userId: id, followId: selfUserId })
+      .count();
+    return total > 0;
+  })();
+
   return {
     ...userInfo,
-    follow,
-    fans,
+    followNum,
+    isFollow,
+    fansNum,
+    isFans,
     isSelf: userInfo._openid === wxContext.OPENID,
   };
 };
